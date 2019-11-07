064564c6b8093183148f9bcccf7506b9741957e9
Göran Sandström <goran.sandstrom@bookbeat.com>
2019-11-07 15:53:01 +0100

    First commit: * Added attention layers * Added Time-Delay Neural Network layers

---
+++ veqtor_keras/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from veqtor_keras import layers
+from veqtor_keras import util
+from veqtor_keras import blocks
+
+# Cleanup symbols to avoid polluting namespace.
+del absolute_import
+del division
+del print_function
---
+++ veqtor_keras/blocks/localized_attention_blocks.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+import tensorflow.keras.layers as tfkl
+from veqtor_keras import layers as vqkl
+
