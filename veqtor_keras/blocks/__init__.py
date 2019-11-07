0946cb8bf4472f3bff092b10ade0c4379b55a223
Göran Sandström <goran.sandstrom@bookbeat.com>
2019-11-07 17:02:21 +0100

    Added layer factory Added simple localized attention block

--- veqtor_keras/blocks/__init__.py
+++ veqtor_keras/blocks/__init__.py
@@ -3,6 +3,7 @@ from __future__ import division
 from __future__ import print_function
 
 from . import localized_attention_blocks
+from . import layer_factory
 
 # Cleanup symbols to avoid polluting namespace.
 del absolute_import
